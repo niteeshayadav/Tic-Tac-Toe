c9a7fb1112b0bf17f9e23ea7ea9dc2a428d407ab
Niteesha Yadav Panchadarla <niteeshayadav66@gmail.com>
2025-10-30 02:38:14 +0530

    Completed Tic Tac Toe game — now fully playable with confetti and winner display!

---
+++ app.js
@@ -0,0 +1,106 @@
+let cells=document.querySelectorAll(".cell");
+let resetBtn=document.querySelector(".reset-btn");
+let Game=document.querySelector(".game");
+
+let message=document.createElement("p");
+message.style.color="rgb(235, 18, 18)";
+message.style.fontSize="3rem";
+message.style.fontFamily = "baloo-2-font,cursive";
+Game.append(message);
+
+const winningPatterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+let turnX=true;
+
+cells.forEach(cell => {
+    cell.addEventListener("click",() => {
+
+        if(cell.innerText !== ""){
+          return;
+        }
+
+        if(turnX){
+          cell.innerText="X";
+          turnX=false;
+
+       }
+       else{
+          cell.innerText="O";
+          turnX=true;
+       }
+
+       checkWinner();
+
+    });
+});
+
+
+const checkWinner = () => {
+    for(let pattern of winningPatterns){
+        let pos1Value=cells[pattern[0]].innerText;
+        let pos2Value=cells[pattern[1]].innerText;
+        let pos3Value=cells[pattern[2]].innerText;
+
+        if(pos1Value !== "" && pos2Value !=="" && pos3Value !==""){
+            if(pos1Value === pos2Value && pos2Value === pos3Value){
+                message.innerText="Player "+pos1Value+" Won !";
+                disableCells();
+
+                confetti({
+                  particleCount: 150,    // number of confetti pieces
+                  spread: 70,            // how wide the burst spreads
+                  origin: { y: 0.6 },    // where it starts (0 = top, 1 = bottom)
+                  colors: ['#ff0a54', '#ff477e', '#ff7096', '#ff85a1', '#fbb1bd']
+                });
+
+                return;
+
+            }
+        }
+    }
+    //Check for Draw
+    let isallFilled=true;
+    cells.forEach(cell => {
+        if(cell.innerText === ""){
+            isallFilled=false;
+        }
+    });
+    if(isallFilled){
+        message.innerText="It's A Draw !";
+        disableCells();
+    }
+};
+resetBtn.onclick = () => {
+    cells.forEach(cell => {
+        cell.innerText="";
+        cell.disabled=false;
+    });
+    message.innerText="";
+    turnX=true;
+};
+
+const disableCells = () => {
+    cells.forEach(cell => {
+        cell.disabled=true;
+    });
+};
+
+/* The upper disableCells() function can also be written as :-
+const disableCells = () => {
+    for(let cell of cells){
+        cell.disabled=true;
+    }
+} 
+This is a traditional practise and old method,modern js use upper method...
+Also .forEach is used for NodeLists like "cells" here and for Arrays but not for strings
+*/
+
