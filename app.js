2abdb8e33a39256c8c23932849c14ded099761cf
Niteesha Yadav Panchadarla <niteeshayadav66@gmail.com>
2025-10-30 15:18:20 +0530

    feat : added player turn indicator and improved mobile responsiveness (320px)

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 let cells=document.querySelectorAll(".cell");
 let resetBtn=document.querySelector(".reset-btn");
 let Game=document.querySelector(".game");
+let Heading=document.querySelector("#heading");
 
 let message=document.createElement("p");
 message.style.color="rgb(235, 18, 18)";
 message.style.fontSize="3rem";
 message.style.fontFamily = "baloo-2-font,cursive";
 Game.append(message);
+
+let playerTurn=document.createElement("p");
+playerTurn.style.color="rgb(235, 18, 18)";
+playerTurn.style.fontSize="2rem";
+playerTurn.style.fontFamily= "baloo-2-font,cursive";
+playerTurn.style.marginTop="0rem";
+playerTurn.style.marginBottom="1.5rem";
+Heading.insertAdjacentElement("afterend",playerTurn);
+/*
+Let’s read it in English:
+“Insert playerTurn after the end of the heading element.”
+*/
+
+/*
+Alternatively we can also use :-
+Game.insertBefore(playerTurn,Heading.nextSibling);
+It means,in Game -insert playerTurn before next sibling of Heading(here container class)...
+so add playerTurn before .container and after Heading
+*/ 
+
 
 const winningPatterns=[
     [0,1,2],
@@ -20,6 +41,7 @@ const winningPatterns=[
 ];
 
 let turnX=true;
+playerTurn.innerText="Player X Turn";
 
 cells.forEach(cell => {
     cell.addEventListener("click",() => {
@@ -31,11 +53,13 @@ cells.forEach(cell => {
         if(turnX){
           cell.innerText="X";
           turnX=false;
+          playerTurn.innerText="Player O Turn";
 
        }
        else{
           cell.innerText="O";
           turnX=true;
+          playerTurn.innerText="Player X Turn";
        }
 
        checkWinner();
@@ -53,6 +77,7 @@ const checkWinner = () => {
         if(pos1Value !== "" && pos2Value !=="" && pos3Value !==""){
             if(pos1Value === pos2Value && pos2Value === pos3Value){
                 message.innerText="Player "+pos1Value+" Won !";
+                playerTurn.innerText="";
                 disableCells();
 
                 confetti({
@@ -86,6 +111,7 @@ resetBtn.onclick = () => {
     });
     message.innerText="";
     turnX=true;
+    playerTurn.innerText="Player X Turn";
 };
 
 const disableCells = () => {
